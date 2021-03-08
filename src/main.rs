03479ac0f5d85b38dc81ff3aea37dcc41c91c0b7
Howard Zhu <zhc_howard@hotmail.com>
2021-03-08 23:42:28 +0800

    add a default background color for game's window

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    render::pass::ClearColor,
+    sprite::collide_aabb::{collide, Collision},
+};
 
 fn main() {
     let mut app = App::build();
     app.add_plugins(DefaultPlugins)
+        .add_resource(ClearColor)
+        .add_startup_system(setup.system())
         .run();
+}
+
+fn setup(
+    commands: &mut Commands,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    asset_server: Res<AssetServer>,
+) {
+    // Add the game's entities to our world
+    commands
+        // cameras
+        .spawn(Camera2dBundle::default())
+        .spawn(CameraUiBundle::default());
 }
