844312e5b75300a38bd9ae6c097fb548ee593dbe
Howard Zhu <zhc_howard@hotmail.com>
2021-03-11 23:30:47 +0800

    add first 'tank' on UI

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use bevy::{
     prelude::*,
     render::pass::ClearColor,
-    sprite::collide_aabb::{collide, Collision},
+    // sprite::collide_aabb::{collide, Collision},
 };
 
 fn main() {
@@ -9,17 +9,36 @@ fn main() {
     app.add_plugins(DefaultPlugins)
         .add_resource(ClearColor)
         .add_startup_system(setup.system())
+        .add_startup_stage("game_setup", SystemStage::single(spawn_tank.system()))
         .run();
+}
+
+struct Tank;
+struct Materials {
+    tank: Handle<ColorMaterial>,
 }
 
 fn setup(
     commands: &mut Commands,
     mut materials: ResMut<Assets<ColorMaterial>>,
-    asset_server: Res<AssetServer>,
+    // asset_server: Res<AssetServer>,
 ) {
     // Add the game's entities to our world
     commands
         // cameras
         .spawn(Camera2dBundle::default())
-        .spawn(CameraUiBundle::default());
+        .insert_resource(Materials {
+            tank: materials.add(Color::rgb(0.7, 0.7, 0.7).into()),
+        });
+    // .spawn(CameraUiBundle::default());
+}
+
+fn spawn_tank(commands: &mut Commands, materials: Res<Materials>) {
+    commands
+        .spawn(SpriteBundle {
+            material: materials.tank.clone(),
+            sprite: Sprite::new(Vec2::new(10.0, 10.0)),
+            ..Default::default()
+        })
+        .with(Tank);
 }
