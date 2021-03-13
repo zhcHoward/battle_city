ebd9c42d39f55bc526e9b2c7553beaeb9d2b1074
Howard Zhu <zhc_howard@hotmail.com>
2021-03-13 23:29:21 +0800

    1. add texture for tanks 2. make tanks move with animation

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,12 @@ fn main() {
         height: GAME_HEIGHT as f32,
         ..Default::default()
     })
-    .add_resource(ClearColor)
+    .add_resource(ClearColor(Color::rgb(0., 0., 0.)))
     .add_startup_system(setup.system())
     .add_startup_stage("game_setup", SystemStage::single(spawn_tank.system()))
     .add_system(tank_movement.system())
     .add_system(position_translation.system())
+    .add_system(animate_sprite_system.system())
     .add_plugins(DefaultPlugins)
     .run();
 }
@@ -31,9 +32,10 @@ struct Materials {
 fn setup(
     commands: &mut Commands,
     mut materials: ResMut<Assets<ColorMaterial>>,
-    // asset_server: Res<AssetServer>,
+    asset_server: Res<AssetServer>,
 ) {
     // Add the game's entities to our world
+
     commands
         // cameras
         .spawn(Camera2dBundle::default())
@@ -43,17 +45,45 @@ fn setup(
     // .spawn(CameraUiBundle::default());
 }
 
-fn spawn_tank(commands: &mut Commands, materials: Res<Materials>) {
+fn animate_sprite_system(
+    time: Res<Time>,
+    texture_atlases: Res<Assets<TextureAtlas>>,
+    mut query: Query<(&mut Timer, &mut TextureAtlasSprite, &Handle<TextureAtlas>)>,
+) {
+    for (mut timer, mut sprite, texture_atlas_handle) in query.iter_mut() {
+        timer.tick(time.delta_seconds());
+        if timer.finished() {
+            // let texture_atlas = texture_atlases.get(texture_atlas_handle).unwrap();
+            if sprite.index % 2 == 0 {
+                sprite.index += 1;
+            } else {
+                sprite.index -= 1;
+            }
+            // sprite.index = ((sprite.index as usize + 1) % texture_atlas.textures.len()) as u32;
+        }
+    }
+}
+
+fn spawn_tank(
+    commands: &mut Commands,
+    // materials: Res<Materials>,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    let texture_handle = asset_server.load("General Sprites.png");
+    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(16., 16.), 25, 16);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
     commands
-        .spawn(SpriteBundle {
-            material: materials.tank.clone(),
-            sprite: Sprite::new(Vec2::new(FULL_BLOCK as f32, FULL_BLOCK as f32)),
+        .spawn(SpriteSheetBundle {
+            texture_atlas: texture_atlas_handle,
+            transform: Transform::from_scale(Vec3::splat(2.)),
             ..Default::default()
         })
         .with(Tank {
             direction: Direction::Up,
         })
-        .with(Position { x: 4, y: 0 });
+        .with(Position { x: 4, y: 0 })
+        .with(Timer::from_seconds(0.1, true));
 }
 
 const BOUNDARY: (u32, u32, u32, u32) = (12, 12, 0, 0); // up, right, down, left
