83963d52903bee165c5e52ad7aad0e186be0f272
Howard Zhu <zhc_howard@hotmail.com>
2021-03-20 22:17:10 +0800

    load texture in setup function; remove animation for now because tank moves only when a direction key is pressed

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ fn main() {
     .add_startup_system(setup.system())
     .add_startup_stage("game_setup", SystemStage::single(spawn_tank.system()))
     .add_system(tank_movement.system())
-    .add_system(animate_sprite_system.system())
+    // .add_system(animate_sprite_system.system())
     .add_plugins(DefaultPlugins)
     .run();
 }
@@ -29,53 +29,57 @@ struct Materials {
     tank: Handle<ColorMaterial>,
 }
 
+struct Textures {
+    texture: Handle<TextureAtlas>,
+}
+
 fn setup(
     commands: &mut Commands,
     mut materials: ResMut<Assets<ColorMaterial>>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
     asset_server: Res<AssetServer>,
 ) {
     // Add the game's entities to our world
+    let texture_handle = asset_server.load("General Sprites.png");
+    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(16., 16.), 25, 16);
+    let texture_handle = texture_atlases.add(texture_atlas);
 
     commands
         // cameras
         .spawn(Camera2dBundle::default())
         .insert_resource(Materials {
             tank: materials.add(Color::rgb(0.7, 0.7, 0.7).into()),
+        })
+        .insert_resource(Textures {
+            texture: texture_handle,
         });
     // .spawn(CameraUiBundle::default());
 }
 
-fn animate_sprite_system(
-    time: Res<Time>,
-    texture_atlases: Res<Assets<TextureAtlas>>,
-    mut query: Query<(&mut Timer, &mut TextureAtlasSprite, &Handle<TextureAtlas>)>,
-) {
-    for (mut timer, mut sprite, texture_atlas_handle) in query.iter_mut() {
-        timer.tick(time.delta_seconds());
-        if timer.finished() {
-            // let texture_atlas = texture_atlases.get(texture_atlas_handle).unwrap();
-            if sprite.index % 2 == 0 {
-                sprite.index += 1;
-            } else {
-                sprite.index -= 1;
-            }
-            // sprite.index = ((sprite.index as usize + 1) % texture_atlas.textures.len()) as u32;
-        }
-    }
-}
+// fn animate_sprite_system(
+//     time: Res<Time>,
+//     texture_atlases: Res<Assets<TextureAtlas>>,
+//     mut query: Query<(&mut Timer, &mut TextureAtlasSprite, &Handle<TextureAtlas>)>,
+// ) {
+//     for (mut timer, mut sprite, texture_atlas_handle) in query.iter_mut() {
+//         timer.tick(time.delta_seconds());
+//         if timer.finished() {
+//             // let texture_atlas = texture_atlases.get(texture_atlas_handle).unwrap();
+//             if sprite.index % 2 == 0 {
+//                 sprite.index += 1;
+//             } else {
+//                 sprite.index -= 1;
+//             }
+//             // sprite.index = ((sprite.index as usize + 1) % texture_atlas.textures.len()) as u32;
+//         }
+//     }
+// }
 
-fn spawn_tank(
-    commands: &mut Commands,
-    // materials: Res<Materials>,
-    asset_server: Res<AssetServer>,
-    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
-) {
-    let texture_handle = asset_server.load("General Sprites.png");
-    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(16., 16.), 25, 16);
-    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+fn spawn_tank(commands: &mut Commands, textures: Res<Textures>) {
     commands
         .spawn(SpriteSheetBundle {
-            texture_atlas: texture_atlas_handle,
+            // sprite: TextureAtlasSprite::new(200),  // P2
+            texture_atlas: textures.texture.clone(),
             transform: Transform {
                 translation: TANK1_SPAWN_POSITION,
                 scale: Vec3::splat(SCALE),
@@ -115,7 +119,7 @@ const SCALE: f32 = 2.;
 const TANK_SPEED: f32 = MIN_BLOCK / 2.;
 const MIN_BLOCK: f32 = 4. * SCALE; // unit: px
 const BLOCK: f32 = 2. * MIN_BLOCK;
-const MAX_BLOCK: f32 = 2. * BLOCK;
+const MAX_BLOCK: f32 = 2. * BLOCK; // tank's size
 const GAME_WIDTH: f32 = 13. * MAX_BLOCK;
 const GAME_HEIGHT: f32 = GAME_WIDTH;
 
