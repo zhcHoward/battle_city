8990d552ad0962f711d28812eadb4d13c841b77d
Howard Zhu <zhc_howard@hotmail.com>
2021-03-25 23:08:03 +0800

    1. add boundaries to the battle field 2. try to add a collision system that forbid tanks to go beyound the boundary    but it seems failed. The movement of tanks towards boundary looks    like they move into the boundary and get pushed back with a spring.    This effect looks funny but is not really what I want.
    It seems that I should check collision in tank movement system instead
    of creating a new colloion system. If collision exists, I shoud forbid
    tank to move forward.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use bevy::{
     math::const_vec3,
-    // sprite::collide_aabb::{collide, Collision},
     prelude::*,
     render::pass::ClearColor,
+    sprite::collide_aabb::{collide, Collision},
 };
 
 mod texture;
@@ -12,8 +12,8 @@ fn main() {
     let mut app = App::build();
     app.add_resource(WindowDescriptor {
         title: "Battle City".to_string(),
-        width: GAME_WIDTH,
-        height: GAME_HEIGHT,
+        width: GAME_WIDTH + 100.,
+        height: GAME_HEIGHT + 100.,
         ..Default::default()
     })
     .add_resource(ClearColor)
@@ -21,6 +21,7 @@ fn main() {
     .add_startup_stage("game_setup", SystemStage::single(spawn_tank.system()))
     .add_system(player_tank_movement.system())
     .add_system(tank_animate_system.system())
+    .add_system(tank_collision_system.system())
     .add_plugins(DefaultPlugins)
     .run();
 }
@@ -38,6 +39,7 @@ struct Tank {
 
 fn setup(
     commands: &mut Commands,
+    mut materials: ResMut<Assets<ColorMaterial>>,
     mut texture_atlases: ResMut<Assets<TextureAtlas>>,
     asset_server: Res<AssetServer>,
 ) {
@@ -50,6 +52,44 @@ fn setup(
         .insert_resource(Textures {
             texture: texture_handle,
         });
+
+    // spawn boundaries
+    let boundary_material = materials.add(Color::default().into());
+    let wall_thickness = 10.0;
+    let bounds = Vec2::new(900.0, 600.0);
+    commands
+        // left
+        .spawn(SpriteBundle {
+            material: boundary_material.clone(),
+            transform: Transform::from_translation(Vec3::new(-GAME_WIDTH / 2. - 5., 0., 0.)),
+            sprite: Sprite::new(Vec2::new(wall_thickness, GAME_HEIGHT + wall_thickness)),
+            ..Default::default()
+        })
+        .with(Collider::Boundary)
+        // right
+        .spawn(SpriteBundle {
+            material: boundary_material.clone(),
+            transform: Transform::from_translation(Vec3::new(GAME_WIDTH / 2. + 5., 0., 0.)),
+            sprite: Sprite::new(Vec2::new(wall_thickness, GAME_HEIGHT + wall_thickness)),
+            ..Default::default()
+        })
+        .with(Collider::Boundary)
+        // top
+        .spawn(SpriteBundle {
+            material: boundary_material.clone(),
+            transform: Transform::from_translation(Vec3::new(0., GAME_HEIGHT / 2. + 5., 0.)),
+            sprite: Sprite::new(Vec2::new(GAME_WIDTH + wall_thickness, wall_thickness)),
+            ..Default::default()
+        })
+        .with(Collider::Boundary)
+        // bottom
+        .spawn(SpriteBundle {
+            material: boundary_material.clone(),
+            transform: Transform::from_translation(Vec3::new(0., -GAME_HEIGHT / 2. - 5., 0.)),
+            sprite: Sprite::new(Vec2::new(GAME_WIDTH + wall_thickness, wall_thickness)),
+            ..Default::default()
+        })
+        .with(Collider::Boundary);
 }
 
 const P1_DIRECTION_KEYS: [KeyCode; 4] = [KeyCode::A, KeyCode::D, KeyCode::W, KeyCode::S];
@@ -127,6 +167,7 @@ fn spawn_tank(commands: &mut Commands, textures: Res<Textures>) {
             direction: Direction::Up,
             owner: Owner::Player1,
         })
+        .with(Collider::Tank)
         .with(Timer::from_seconds(0.1, true));
 
     // spawn P2's tank
@@ -145,6 +186,7 @@ fn spawn_tank(commands: &mut Commands, textures: Res<Textures>) {
             direction: Direction::Up,
             owner: Owner::Player2,
         })
+        .with(Collider::Tank)
         .with(Timer::from_seconds(0.1, true));
 }
 
@@ -194,16 +236,16 @@ fn player_tank_movement(
                     }
                 }
                 if keyboard_input.pressed(KeyCode::W) {
-                    if tank.direction == Direction::Up {
-                        if transform.translation.y < BOUNDARY {
-                            transform.translation.y += TANK_SPEED;
-                        }
-                    } else {
-                        if keyboard_input.just_pressed(KeyCode::W) {
-                            sprite.index = 0;
-                            tank.direction = Direction::Up;
-                        }
+                    // if tank.direction == Direction::Up {
+                    //     if transform.translation.y < BOUNDARY {
+                    transform.translation.y += TANK_SPEED;
+                    //     }
+                    // } else {
+                    if keyboard_input.just_pressed(KeyCode::W) {
+                        sprite.index = 0;
+                        tank.direction = Direction::Up;
                     }
+                    // }
                 }
             }
             Owner::Player2 => {
@@ -290,3 +332,52 @@ impl Direction {
 const TANK1_SPAWN_POSITION: Vec3 =
     const_vec3!([-2. * MAX_BLOCK, (MAX_BLOCK - GAME_WIDTH) / 2., 0.]);
 const TANK2_SPAWN_POSITION: Vec3 = const_vec3!([2. * MAX_BLOCK, (MAX_BLOCK - GAME_WIDTH) / 2., 0.]);
+
+enum Collider {
+    Boundary, // boundary of battle field
+    Brick,
+    Iron,
+    River,
+    Grass,
+    Snow,
+    Base, // The eagle
+    Tank,
+    Bullet,
+}
+
+fn tank_collision_system(
+    mut tank_query: Query<&mut Transform, With<Tank>>,
+    collider_query: Query<(&Collider, &Transform, &Sprite)>,
+) {
+    for mut tank_transform in tank_query.iter_mut() {
+        for (collider, transform, sprite) in collider_query.iter() {
+            let collision = collide(
+                tank_transform.translation,
+                Vec2::new(16., 16.),
+                transform.translation,
+                sprite.size,
+            );
+
+            if let Some(collision) = collision {
+                println!("in collision: {:?}", collision);
+                if let Collider::Boundary = *collider {
+                    tank_transform.translation.y = BOUNDARY;
+                }
+            }
+        }
+    }
+}
+
+// fn player_tank_shooting(
+//     keyboard_input: Res<Input<KeyCode>>,
+//     commands: &mut Commands,
+//     query: Query<&Tank>,
+// ) {
+//     for tank in query.iter() {
+//         match tank.owner {
+//             Owner::Player1 => {}
+//             Owner::Player2 => {}
+//             Owner::AI => (),
+//         }
+//     }
+// }
